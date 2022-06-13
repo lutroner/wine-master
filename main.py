53fc0c4518ef1a7805dcd777c7808e85b6d06ed5
lutroner <102256437+lutroner@users.noreply.github.com>
2022-06-13 15:19:30 +0300

    Fix variable names

--- main.py
+++ main.py
@@ -9,23 +9,23 @@ env = Environment(
 )
 
 template = env.get_template('template.html')
-wine_data = read_excel('wine.xlsx', sheet_name='Лист1')
-open_date = datetime(year=1920, month=1, day=1)
-wine_dict = wine_data.to_dict(orient='records')
-category_list = list(dict.fromkeys(wine_data['Категория'].to_list()))
+drinks_dataset = read_excel('wine.xlsx', sheet_name='Лист1')
+foundation_date = datetime(year=1920, month=1, day=1)
+drinks = drinks_dataset.to_dict(orient='records')
+drinks_categories = list(dict.fromkeys(drinks_dataset['Категория'].to_list()))
 
 # Избавляемся от non в значениях словаря
-for di in wine_dict:
-    for key, item in di.items():
-        if str(di[key]) == 'nan':
-            di[key] = ''
+for drink in drinks:
+    for key, item in drink.items():
+        if str(drink[key]) == 'nan':
+            drink[key] = ''
 
 rendered_page = template.render(
-    current_time=f'Уже {datetime.now().year - open_date.year} года с вами',
+    current_time=f'Уже {datetime.now().year - foundation_date.year} года с вами',
     # Преобразуем словарь в новый с категориями в качестве ключей
-    wine_category_list={category: [wine for wine in wine_dict
-                                   if wine['Категория'] == category]
-                        for category in category_list}
+    wine_category_list={category: [drink for drink in drinks
+                                   if drink['Категория'] == category]
+                        for category in drinks_categories}
 )
 
 with open('index.html', 'w', encoding="utf8") as file:
